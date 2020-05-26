f1ecc3c32ada14ccffd035564b374b7ebdf2a436
Alain Saint-Dic <alains214@gmail.com>
2020-05-26 23:32:05 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,3 +1,16 @@
+def say_hello_ten_times
+  phrase = "Hello World"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
