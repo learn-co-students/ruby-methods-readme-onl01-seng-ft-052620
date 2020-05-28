310bcf925eae341065009dee664c6023f4dfeaa0
Julie Garcia <juliegarcia.cali@gmail.com>
2020-05-28 23:52:03 +0000

    Done.

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ see what it produces.
 
 ## Objectives
 
+
 - Describe how methods can define new routines and procedures for our code.
 - Define a method with the `def` keyword, supply the method's body, and close
   the method definition with the `end` keyword.
--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,19 @@
 # Use this space to code along with the readme
+def greeting
+puts = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+say_hello_ten_times
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+greeting
+
+greeting
